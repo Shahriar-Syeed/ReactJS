2af3ce082a42479519fd5af35410b569a92d2682
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-06-05 21:10:42 +0600

    styled upto telwind

--- Style-react/src/components/AuthInputs.jsx
+++ Style-react/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { styled } from "styled-components";
+import StyledButton from "./Button.jsx";
+import Input from "./Input.jsx"
 
 const ControlContainer = styled.div`
   display: flex;
@@ -7,44 +9,7 @@ const ControlContainer = styled.div`
   gap: 0.5rem;
   margin-bottom: 1.5rem;
 `;
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  border: 1px solid;
-  color: ${(props) => (props.$invalid ? "#ef4444" : "#374151")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  background-color: ${(props) => (props.$invalid ? " #fed2d2" : "#d1d5db")};
-  background-color: ${({ $invalid }) => ($invalid ? " #fed2d2" : "#d1d5db")};
-  border-color: ${(props) => (props.$invalid ? " #f73f3f" : "transparent")};
-  border-color: ${({ $invalid }) => ($invalid ? " #f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-const StyledButton = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
 
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
 const TextButton = styled.button`
   color: #f0b322;
   border: none;
@@ -53,6 +18,7 @@ const TextButton = styled.button`
     color: #f0920e;
   }
 `;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -76,33 +42,35 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
+        {/* <p>
           <Label
             $invalid={emailNotValid}
             // className={`label ${emailNotValid ? "invalid" : undefined}`}
           >
             Email
-          </Label>
+          </Label> */}
           <Input
+          label="Email"
             type="email"
-            $invalid={emailNotValid}
+            invalid={emailNotValid}
             // style={{
             //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db"
             // }}
             // className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
-        </p>
-        <p>
+        {/* </p> */}
+        {/* <p>
           <Label
             // className={`label ${passwordNotValid ? "invalid" : undefined}`}
             $invalid={passwordNotValid}
           >
             Password
-          </Label>
+          </Label> */}
           <Input
+          label="Password"
             type="password"
-            $invalid={passwordNotValid}
+            invalid={passwordNotValid}
             // style={{
             //   backgroundColor: passwordNotValid ? "#fed2d2" : "#d1d5db"
             // }}
@@ -111,7 +79,7 @@ export default function AuthInputs() {
               handleInputChange("password", event.target.value)
             }
           />
-        </p>
+        {/* </p> */}
       </ControlContainer>
       <div className="actions">
         <TextButton type="button">
