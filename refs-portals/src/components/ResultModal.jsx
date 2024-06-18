7ae826c4e0d13be45eec3e8a390389a8f6979ff8
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-06-18 21:31:21 +0600

    use-ref

---
+++ refs-portals/src/components/ResultModal.jsx
@@ -0,0 +1,36 @@
+// export default function ResultModal({ref, result, targetTime}){
+//     return (
+//         <dialog className="result-modal" refs={ref} >
+//             <h2>You {result}! </h2>
+//             <p>The target time was <strong>{targetTime} second{targetTime > 1 && 's'}</strong></p>
+//             <p>You stopped the timer with <strong> X seconds left.</strong></p>
+//             <form method="dialog">
+//                 <button>Close</button>
+//             </form>
+//         </dialog>
+//     )
+// }
+import { forwardRef } from "react";
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  return (
+    <dialog className="result-modal" ref={ref}>
+      <h2>You {result}! </h2>
+      <p>
+        The target time was{" "}
+        <strong>
+          {targetTime} second{targetTime > 1 && "s"}
+        </strong>
+      </p>
+      <p>
+        You stopped the timer with <strong> X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+export default ResultModal;
