ac69f54cb738941e7b4ad26d203381fcd1bb00a9
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-06-27 16:00:03 +0600

    project list as button, useRef and forwardUseRef input

--- management-app/src/App.jsx
+++ management-app/src/App.jsx
@@ -4,32 +4,46 @@ import ProjectSidebar from "./components/ProjectSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
-
 function App() {
-  const [projectsState, setProjectsState]= useState({
+  const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    project: [],
+    projects: [],
   });
 
-  function handleStartAddProject(){
-    setProjectsState(prevState =>{
-      return{
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
         ...prevState,
         selectedProjectId: null,
-
-      }
-    })
+      };
+    });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...projectsState.projects, newProject],
+      };
+    });
+  }
+  console.log(projectsState);
   let content;
-  if(projectsState.selectedProjectId === null){
-    content = <NewProject />;
-  }else if (projectsState.selectedProjectId === undefined){
-  content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
-}
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
       {content}
     </main>
   );
