c642db653b7f14fa2dd432ecc1ba3a3a6227be84
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-07-04 16:17:21 +0600

    useEffect travel apps

--- test/contextToggleTheme/src/App.jsx
+++ test/contextToggleTheme/src/App.jsx
@@ -10,44 +10,57 @@
 // }
 
 // export default App;
+// import React from 'react';
+
+
+// export function counterReducer(state, action) {
+//     if(action.type === 'INCREMENT'){      
+      
+//         return {counter: state.counter++};
+//     }
+//     if(action.type === 'DECREMENT'){
+//        return {counter: state.counter--}
+//     }
+//     if(action.type === 'RESET'){
+       
+//         return {counter: 0};
+//     }
+// }
+
+// function App() {
+//     const [counterState, counterDispath]= React.useReducer(counterReducer, {
+//         counter: 0,
+//     });
+//   return (
+//     <div id="app">
+//       <h1>The (Final?) Counter</h1>
+//       <p id="actions">
+//         <button onClick={()=>counterDispath({
+//             type: 'INCREMENT',
+//         })}>Increment</button>
+//         <button onClick={()=>counterDispath({
+//             type: 'DECREMENT',
+//         })}>Decrement</button>
+//         <button onClick={()=>counterDispath({
+//             type: 'RESET',
+//         })}>Reset</button>
+//       </p>
+//       <p id="counter">{counterState.counter}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 
-
-export function counterReducer(state, action) {
-    if(action.type === 'INCREMENT'){      
-      
-        return {counter: state.counter++};
-    }
-    if(action.type === 'DECREMENT'){
-       return {counter: state.counter--}
-    }
-    if(action.type === 'RESET'){
-       
-        return {counter: 0};
-    }
+export default class App extends React.Component {
+  
+  render(){
+    return(
+      <div  className='app'>
+        <div>We shall learn useEffect() today</div>
+      </div>
+    );
+  }
 }
-
-function App() {
-    const [counterState, counterDispath]= React.useReducer(counterReducer, {
-        counter: 0,
-    });
-  return (
-    <div id="app">
-      <h1>The (Final?) Counter</h1>
-      <p id="actions">
-        <button onClick={()=>counterDispath({
-            type: 'INCREMENT',
-        })}>Increment</button>
-        <button onClick={()=>counterDispath({
-            type: 'DECREMENT',
-        })}>Decrement</button>
-        <button onClick={()=>counterDispath({
-            type: 'RESET',
-        })}>Reset</button>
-      </p>
-      <p id="counter">{counterState.counter}</p>
-    </div>
-  );
-}
-
-export default App;
---
+++ test/contextToggleTheme/src/compnents/MyComponentClass.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class MyComponent extends React.Component{
+    state ={
+        count: 0,
+        date: new Date(),
+      };
+
+    render(){
+          const { date } = this.state;
+
+        return (
+            <div>
+                <p>Time: {date.toLocaleTimeString()}</p>
+                <p>
+                    <button type='button'>Click</button>
+                </p>
+            </div>
+        );
+    }
+}
+export default MyComponent;
