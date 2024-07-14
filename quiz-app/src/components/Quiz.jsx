b68f4997081d68729f2964537d11e880cdddd8fc
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-07-14 22:59:49 +0600

    shuffled answer in useRef

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 import QUESTIONS from "../questions.js";
 
 import QuestionTimer from "../QuestionTimer.jsx";
@@ -6,22 +6,24 @@ import quizComplete from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0); // redundant due to can be find the indx from the length of array answers
+  const shuffledAnswers =  useRef();
   const [answerState,setAnswerState]=useState('unanswered');
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = answerState === 'unanswered' ? userAnswers.length : userAnswers.length -1;
+  const activeQuestionIndex = (answerState === 'unanswered') ? userAnswers.length : userAnswers.length -1;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
   console.log("quizIsComplete", quizIsComplete);
 
+  console.log(userAnswers);
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
 
     setAnswerState('answered');
-    
+
     setUserAnswers((prevAnswer) => [...prevAnswer, selectedAnswer]);
-    // console.log(userAnswers);
+    console.log(userAnswers);
 
     setTimeout(()=>{
       if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
@@ -49,32 +51,39 @@ export default function Quiz() {
     );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  // const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+if(!shuffledAnswers.current){//if shuffledAnswers. current is undefined
+  shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.current.sort(() => Math.random() - 0.5);
+}
 
   return (
     <div id="quiz">
       <div id="question">
         <QuestionTimer
           key={activeQuestionIndex} //to recreate and again progressbar counting
-          timeout={4000}
+          timeout={15000}
           onTimeout={handleSkipAnswer}
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map((answer) => {
-
+          {shuffledAnswers.current.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length -1] === answer;
             let cssClass ='';
 
-            if(answerState === 'answered'){}
-            if(answerState === 'correct'){}
+            if(answerState === 'answered' && isSelected){
+              cssClass = 'selected'
+            }
+            if((answerState === 'correct' || answerState === 'wrong')&& isSelected){
+              cssClass=answerState;
+            }
 
 
-            return <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)} className={'hi'}>
+            return (<li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
                 {answer}
               </button>
-            </li>
+            </li>);
           })}
         </ul>
       </div>
