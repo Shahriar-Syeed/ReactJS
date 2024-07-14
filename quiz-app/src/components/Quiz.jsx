7653fe8408e46b4def66be3ad99dcebfc5af3b84
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-07-14 06:30:53 +0600

    correct answer or wrong answered , answerState process start

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -6,9 +6,10 @@ import quizComplete from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0); // redundant due to can be find the indx from the length of array answers
+  const [answerState,setAnswerState]=useState('unanswered');
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex = answerState === 'unanswered' ? userAnswers.length : userAnswers.length -1;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
   console.log("quizIsComplete", quizIsComplete);
@@ -16,10 +17,24 @@ export default function Quiz() {
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
+
+    setAnswerState('answered');
+    
     setUserAnswers((prevAnswer) => [...prevAnswer, selectedAnswer]);
     // console.log(userAnswers);
+
+    setTimeout(()=>{
+      if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+        setAnswerState('correct');
+      }else {
+        setAnswerState('wrong');
+      }
+      setTimeout(()=>{
+        setAnswerState('unanswered')
+      },2000)
+    }, 1000);
   },
-  []);
+  [activeQuestionIndex]);
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
     [handleSelectAnswer]
@@ -47,13 +62,20 @@ export default function Quiz() {
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
+          {shuffledAnswers.map((answer) => {
+
+            let cssClass ='';
+
+            if(answerState === 'answered'){}
+            if(answerState === 'correct'){}
+
+
+            return <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)} className={'hi'}>
                 {answer}
               </button>
             </li>
-          ))}
+          })}
         </ul>
       </div>
     </div>
