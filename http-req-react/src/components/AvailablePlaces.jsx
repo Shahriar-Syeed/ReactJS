53c98f34b2a7806ceae8b89873849c9475a98508
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-07-30 17:01:50 +0600

    error handle with try and catch and error message

--- http-req-react/src/components/AvailablePlaces.jsx
+++ http-req-react/src/components/AvailablePlaces.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 const places =localStorage.getItem('places'); //syncronizly
 
@@ -7,18 +8,33 @@ export default  function AvailablePlaces({ onSelectPlace }) {
   // Todo: Fetch available places from backend API and install nmp in backend
     const [isFetching, setIsFetching] = useState(true);
     const [availablePlaces, setAvailablePlaces]= useState([]);
+    const [error, setError]= useState();
 
     useEffect(()=>{
       async function fetchingPlaces(){
         setIsFetching(true);
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
-        setAvailablePlaces(resData.places);
+
+        try{
+
+          const response = await fetch('http://localhost:3000/placess');
+          const resData = await response.json();
+  
+          if(!response.ok){ //true 200, 300, false 400, 500 status code
+            // const error = new Error('Failed to fetch places');
+            // throw error;
+            throw new Error('Failed to fetch places');
+          }
+          setAvailablePlaces(resData.places);
+        }catch(error){
+          setError({message: error.message || 'could not fetch places, please try again later.'});
+        }
         setIsFetching(false);
       }
       fetchingPlaces();
     }, []);
-
+    if(error){
+      return <Error title='An error occurred!' message={error.message}/>
+    }
 
   return (
     <Places
