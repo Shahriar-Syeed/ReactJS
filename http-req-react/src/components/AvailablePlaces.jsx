d6b71bd93208c63479450150452dae8926c7bf8e
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-07-30 15:57:35 +0600

    loading fallback text

--- http-req-react/src/components/AvailablePlaces.jsx
+++ http-req-react/src/components/AvailablePlaces.jsx
@@ -5,14 +5,16 @@ const places =localStorage.getItem('places'); //syncronizly
 
 export default  function AvailablePlaces({ onSelectPlace }) {
   // Todo: Fetch available places from backend API and install nmp in backend
+    const [isFetching, setIsFetching] = useState(true);
     const [availablePlaces, setAvailablePlaces]= useState([]);
 
     useEffect(()=>{
       async function fetchingPlaces(){
-
+        setIsFetching(true);
         const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
         setAvailablePlaces(resData.places);
+        setIsFetching(false);
       }
       fetchingPlaces();
     }, []);
@@ -22,6 +24,8 @@ export default  function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
