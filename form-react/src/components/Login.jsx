c7307efb8fe7fb55a869ab116b743149c4dc8e14
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-03 03:03:19 +0600

    login small form complete and start signup big form with new FormData

--- form-react/src/components/Login.jsx
+++ form-react/src/components/Login.jsx
@@ -1,22 +1,39 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  const [enterEmail, setEnterEmail]= useState('');
-  const [enterPassword, setEnterPassword]= useState('');
+  // const [enterEmail, setEnterEmail]= useState('');
+  // const [enterPassword, setEnterPassword]= useState('');
+  const [enteredValue, setEnteredValue]= useState({
+    email : '',
+    password : '',
+  }); 
+
+  const mail = useRef();
+  const pass = useRef();
+ 
 
   function handleSubmit(event){
     event.preventDefault();
 
-    console.log('user email:'+ enterEmail);
+    // console.log('enteredValue', enteredValue.email, enteredValue.password);
+    const enteredEmail = mail.current.value;
+    const enteredPassword = pass.current.value;
+    console.log(enteredEmail, enteredPassword);
+ 
   }
+  function handleEnteredValue(id, input){
+    setEnteredValue({
+      ...enteredValue,
+      [id] :input,
+    })
 
-  function handleEmail(event){
-   setEnterEmail(event.target.value);
   }
-  function handlePassword(event){
-    setEnterPassword(event.target.value);
-   
-  }
+  // function handleEmail(event){
+  //  setEnterEmail(event.target.value);
+  // }
+  // function handlePassword(event){
+  //   setEnterPassword(event.target.value);   
+  // }
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -24,12 +41,14 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={handleEmail} value={enterEmail} />
+          {/* <input id="email" type="email" name="email" onChange={(event)=>handleEnteredValue('email', event.target.value)} value={enteredValue.email} /> */}
+          <input id="email" type="email" name="email" ref={mail} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={handlePassword} value={enterPassword} />
+          {/* <input id="password" type="password" name="password" onChange={(event)=>handleEnteredValue('password', event.target.value)} value={enteredValue.password} /> */}
+          <input id="password" type="password" name="password" ref={pass} />
         </div>
       </div>
 
