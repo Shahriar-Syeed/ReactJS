e16f52d133f0f5b2e8b505cb0282da6aa2ad58a0
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-15 17:20:41 +0600

    adding  a reusable Modal component with useEffect and Opening the cart in the modal via a new Context

--- food-order-app/src/App.jsx
+++ food-order-app/src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header/>
-      <main>
-        <Meals/>
-      </main>
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <main>
+          <Meals />
+        </main>
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
--- food-order-app/src/components/Cart.jsx
+++ food-order-app/src/components/Cart.jsx
@@ -1,18 +1,28 @@
 import { useContext } from "react";
 import Button from "./UI/Button.jsx";
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartData from "./CartData.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  function handleShowCart(){
+    userProgressCtx.showCart();
+  }
 
   const totalCartItems = cartCtx.items.reduce((totalCartItems, item) => {
     return totalCartItems + item.quantity;
   }, 0);
   return (
+    <>
     <nav>
-      <Button textOnly>
+      <Button textOnly onClick={handleShowCart}>
         Cart<span>({totalCartItems})</span>
       </Button>
     </nav>
+    <CartData/>
+    </>
   );
 }
