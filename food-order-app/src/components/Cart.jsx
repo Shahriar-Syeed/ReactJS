290b86855c9db2351e021b88e8fee1a5eb768038
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-15 14:26:10 +0600

    adding to cart number wit reduce((acc, cur)=>{},0), js function

--- food-order-app/src/components/Cart.jsx
+++ food-order-app/src/components/Cart.jsx
@@ -1,10 +1,18 @@
-import Button from "./UI/Button";
+import { useContext } from "react";
+import Button from "./UI/Button.jsx";
+import CartContext from "../store/CartContext.jsx";
 
-export default function Cart(){
-    return(
-        <nav>
-            <Button textOnly>Cart<span>(0)</span></Button>            
-        </nav>
-    );
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
 
+  const totalCartItems = cartCtx.items.reduce((totalCartItems, item) => {
+    return totalCartItems + item.quantity;
+  }, 0);
+  return (
+    <nav>
+      <Button textOnly>
+        Cart<span>({totalCartItems})</span>
+      </Button>
+    </nav>
+  );
 }
