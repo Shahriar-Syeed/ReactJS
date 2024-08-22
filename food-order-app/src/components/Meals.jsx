f0a47affdf05dedbf0eb02ff7d41ab877ad1f6df
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-22 17:55:02 +0600

    http request custom hook useHttp and use in Meals component

--- food-order-app/src/components/Meals.jsx
+++ food-order-app/src/components/Meals.jsx
@@ -1,18 +1,31 @@
-import { useState, useEffect } from "react";
-import { fetchAvailableMeals } from "../http.js";
+// import { useState, useEffect } from "react";
+import useHttp from "../hooks/useHttp.js";
+// import { fetchAvailableMeals } from "../http.js";
 import MealItem from "./MealItem.jsx";
 
+const requestConfig ={};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      try {
-        const meals = await fetchAvailableMeals();
-        setLoadedMeals(meals);
-      } catch {}
-    }
-    fetchMeals();
-  }, []);
+  const { data:loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals/',requestConfig, []);
+  console.log(error);
+  console.log("loadedMeals", loadedMeals);
+  
+  // const [loadedMeals, setLoadedMeals] = useState([]);
+  // useEffect(() => {
+  //   async function fetchMeals() {
+  //     try {
+  //       const meals = await fetchAvailableMeals();
+  //       setLoadedMeals(meals);
+  //     } catch {}
+  //   }
+  //   fetchMeals();
+  // }, []);
+  if (isLoading){
+    return (<p>Fetching meals...</p>);
+  }
+  if(error){
+    return (<p>No meals found...</p>);
+  }
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
