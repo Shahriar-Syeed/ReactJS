d88f422737e51564eeab4b014175fb752dd697ba
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-27 13:14:42 +0600

    redux dispatching from new slice of createSlice, configureStore, useSelector, useDispatch

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -4,13 +4,18 @@ import './App.css';
 import Counter from './components/Counter.jsx';
 import Header from './components/Header.jsx';
 import Auth from './components/Auth.jsx';
+import UserProfile from './components/UserProfile.jsx';
+import { useSelector } from 'react-redux';
 function App() {
+
+  const autValue = useSelector(state =>state.auth.isAuthenticated);
+  // authActions();
 
 
   return (
     <>
     <Header/>
-    <Auth/>
+    {autValue ? <UserProfile/>:<Auth/>} 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
