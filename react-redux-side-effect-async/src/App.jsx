c9be21821ddd94af6ae251e59928683ead83c641
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-28 12:31:08 +0600

    using useEffect with redux

--- react-redux-side-effect-async/src/App.jsx
+++ react-redux-side-effect-async/src/App.jsx
@@ -3,9 +3,18 @@ import Layout from './components/Layout/Layout.jsx';
 import Cart from './components/Cart/Cart.jsx';
 import Products from './components/Shop/Products.jsx'
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
   const uiDisplay = useSelector(state => state.ui.cartIsVisible);
+  const cartValue = useSelector(state => state.cart);
+
+  useEffect(()=> {
+    fetch('https://react-redux-cb271-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',{
+      method:'PUT', body: JSON.stringify(cartValue),
+    });
+
+  }, [cartValue]);
   return (
     <>
       <Layout>
