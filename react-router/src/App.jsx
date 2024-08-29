534fba3323e9da3b7e4f4b0e45d34ab01d29aa2a
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-29 14:27:35 +0600

    npm install react-router-dom

---
+++ react-router/src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import HomePage from './pages/Home.jsx';
+// import ProductsPage from './pages/ProductsPage.jsx';
+
+
+// https://example.com
+
+// const router = createBrowserRouter([
+//   {path: '/', element: <HomePage/> },
+//   {path: '/products', element: <ProductsPage/> },
+// ]);
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Hello</h1>
+    {/* <RouterProvider router={router}/> */}
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
---
+++ react-router/src/pages/ProductsPage.jsx
@@ -0,0 +1,7 @@
+
+
+export default function ProductsPage() {
+  return (
+    <div style={{padding: '20px', background: 'skyblue',}}><h1>My Products Page</h1></div>
+  )
+}
