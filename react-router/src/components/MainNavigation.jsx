1afa8f6d9c082aed2bdf2999a9b4e752c5015193
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-08-29 18:29:56 +0600

    Working with navigation link with NavLink

--- react-router/src/components/MainNavigation.jsx
+++ react-router/src/components/MainNavigation.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
@@ -7,10 +7,31 @@ export default function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({isActive}) =>
+                isActive ? classes.active : ''
+              }
+              style={({isActive}) =>
+                ({fontWeight: isActive ? '900' : '400',})
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({isActive}) =>
+                isActive ? classes.active : ''
+              }
+              style={({isActive}) =>
+                ({fontWeight: isActive ? '900' : '400',})
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
