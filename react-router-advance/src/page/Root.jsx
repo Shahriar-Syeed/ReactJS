33c71879e4f2d2173cc4cca0df7ea314472d486c
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-09-11 18:16:45 +0600

    the current navigation state in the UI

--- react-router-advance/src/page/Root.jsx
+++ react-router-advance/src/page/Root.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation.jsx";
 
 export default function RootLayout() {
+  const navigation =useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === 'loading' && <p>Loading ...</p>}
         <Outlet />
       </main>
     </>
